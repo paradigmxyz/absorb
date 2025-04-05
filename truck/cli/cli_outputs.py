b3de1a7445d4edadc0bc41d6b83089aa53d094c0
sslivkoff <stormslivkoff@gmail.com>
2025-04-05 20:16:35 +0000

    improve output formats

---
+++ truck/cli/cli_outputs.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+import truck
+
+
+bullet_styles = {
+    'key_style': 'white bold',
+    'bullet_style': 'green',
+    'colon_style': 'green',
+}
+
+
+def _print_title(title: str) -> None:
+    import rich
+
+    rich.print('[bold green]' + title + '[/bold green]')
+
+
+def _dataset_to_str(dataset: truck.TrackedTable) -> str:
+    return dataset['source_name'] + '.' + dataset['table_name']
+
+
+def _print_dataset_bullet(dataset: truck.TrackedTable) -> None:
+    import toolstr
+
+    toolstr.print_bullet(
+        '[white bold]' + _dataset_to_str(dataset) + '[/white bold]',
+        **bullet_styles,
+    )
+
+
+def _print_source_datasets_bullet(
+    source: str, datasets: list[truck.TrackedTable]
+) -> None:
+    import toolstr
+
+    datasets = [cls.class_name(snake=True) for cls in datasets]
+    toolstr.print_bullet(
+        key=source,
+        value='[green],[/green] '.join(datasets),
+        **bullet_styles,
+    )
+
