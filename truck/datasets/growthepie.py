e31dd6c13cdf37591654268a9ce9a3f3d93504bc
sslivkoff <stormslivkoff@gmail.com>
2025-04-07 07:59:23 +0000

    implement get_available_range for growthepie

--- truck/datasets/growthepie.py
+++ truck/datasets/growthepie.py
@@ -13,6 +13,7 @@ if typing.TYPE_CHECKING:
 class Fundamentals(truck.Table):
     source = 'growthepie'
     write_range = 'overwrite'
+    range_format = 'date_range'
 
     def get_schema(self) -> dict[str, pl.Datatype]:
         return dict(self.collect(None).schema)
@@ -30,3 +31,17 @@ class Fundamentals(truck.Table):
             .pivot(on='metric_key', index=['date', 'network'], values='value')
             .sort('date', 'network')
         )
+
+    def get_available_range(self) -> typing.Any:
+        import datetime
+        import requests
+
+        first = datetime.datetime(year=2021, month=6, day=1)
+
+        url = 'https://api.growthepie.xyz/v1/fundamentals.json'
+        response = requests.get(url)
+        data = response.json()
+        last_str: str = pl.DataFrame(data)['date'].max()  # type: ignore
+        last = datetime.datetime.strptime(last_str, '%Y-%m-%d')
+
+        return [first, last]
