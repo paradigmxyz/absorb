fc8a582f64f3d85d9888d14a8bfcdb1dff5eaae9
sslivkoff <stormslivkoff@gmail.com>
2025-05-06 06:57:23 +0000

    refactor collection methods

--- truck/types/table_class/table_collect.py
+++ truck/types/table_class/table_collect.py
@@ -12,21 +12,8 @@ if typing.TYPE_CHECKING:
 
 
 class TableCollect(table_coverage.TableCoverage):
-    #
-    # # each table should implement collect_chunk or async_collect_chunk
-    #
-
     def collect_chunk(self, data_range: typing.Any) -> pl.DataFrame | None:
         raise NotImplementedError()
-
-    async def async_collect_chunk(
-        self, data_range: typing.Any
-    ) -> pl.DataFrame | None:
-        raise NotImplementedError()
-
-    #
-    # # the rest of the methods will be the same for every table
-    #
 
     def collect(
         self,
@@ -36,68 +23,85 @@ class TableCollect(table_coverage.TableCoverage):
         verbose: int = 1,
         dry: bool = False,
     ) -> None:
+        # get collection plan
         chunk_ranges, paths = self._get_chunks_to_collect(data_range, overwrite)
+        if not overwrite:
+            chunk_ranges, paths = self._filter_existing_chunks(
+                chunk_ranges=chunk_ranges, paths=paths
+            )
+
+        # summarize collection
         if verbose >= 1:
-            self.summarize_collection_plan(
+            self._summarize_collection_plan(
                 chunk_ranges=chunk_ranges,
                 paths=paths,
                 overwrite=overwrite,
                 verbose=verbose,
             )
+
+        # return early if dry
         if dry:
             return None
+
+        # collect each chunk
         for chunk_range, path in zip(chunk_ranges, paths):
             if verbose >= 1:
-                self.summarize_chunk(chunk_range, path)
+                self._summarize_chunk(chunk_range, path)
             df = self.collect_chunk(data_range=chunk_range)
-            if df is None:
-                if verbose >= 1:
-                    import os
-
-                    print('could not collect data for', os.path.basename(path))
-            else:
+            if df is not None:
                 truck.ops.write_file(df=df, path=path)
-
-    async def async_collect(
-        self,
-        data_range: typing.Any | None = None,
-        overwrite: bool = False,
-        verbose: int = 1,
-        dry: bool = False,
-    ) -> None:
-        import asyncio
-
-        chunk_ranges, paths = self._get_chunks_to_collect(data_range, overwrite)
-        if verbose >= 1:
-            self.summarize_collection_plan(
-                chunk_ranges=chunk_ranges,
-                paths=paths,
-                overwrite=overwrite,
-                verbose=verbose,
-            )
-        if dry:
-            return None
-        coroutines = [
-            self._async_process_chunk(chunk_range, path, verbose)
-            for chunk_range, path in zip(chunk_ranges, paths)
-        ]
-        await asyncio.gather(*coroutines)
-
-    async def _async_process_chunk(
-        self, data_range: typing.Any | None, path: str, verbose: int
-    ) -> None:
-        if verbose >= 1:
-            self.summarize_chunk(data_range, path)
-        df = await self.async_collect_chunk(data_range=data_range)
-        if df is not None:
-            truck.ops.write_file(df=df, path=path)
-        else:
             if verbose >= 1:
-                import os
+                self._summarize_collected_chunk(df, chunk_range, path)
 
-                print('could not collect data for', os.path.basename(path))
+    def _get_chunks_to_collect(
+        self, data_range: typing.Any | None = None, overwrite: bool = False
+    ) -> tuple[list[typing.Any], list[str]]:
+        if self.write_range == 'overwrite_all':
+            chunk_ranges = [None]
+            paths = [self.get_file_path(None)]
+        else:
+            if data_range is None:
+                data_ranges = self._get_missing_data_ranges(overwrite)
+            else:
+                data_ranges = [data_range]
+            chunk_ranges = truck.ops.ranges.partition_into_chunks(
+                data_ranges, range_format=self.range_format
+            )
+            paths = [
+                self.get_file_path(data_range=chunk_range)
+                for chunk_range in chunk_ranges
+            ]
+        return chunk_ranges, paths
 
-    def summarize_collection_plan(
+    def _get_missing_data_ranges(self, overwrite: bool) -> list[typing.Any]:
+        if overwrite:
+            return [self.get_available_range()]
+        else:
+            collected_range = self.get_collected_range()
+            available_range = self.get_available_range()
+            if collected_range is None:
+                return [available_range]
+            else:
+                return truck.ops.ranges.get_range_diff(
+                    subtract_this=collected_range,
+                    from_this=available_range,
+                    range_format=self.range_format,
+                )
+
+    def _filter_existing_chunks(
+        self, chunk_ranges: list[typing.Any], paths: list[str]
+    ) -> tuple[list[typing.Any], list[str]]:
+        import os
+
+        new_chunk_ranges: list[typing.Any] = []
+        new_paths = []
+        for chunk_range, path in zip(chunk_ranges, paths):
+            if not os.path.isfile(path):
+                new_chunk_ranges.append(chunk_range)
+                new_paths.append(path)
+        return new_chunk_ranges, new_paths
+
+    def _summarize_collection_plan(
         self,
         chunk_ranges: list[typing.Any],
         paths: list[str],
@@ -141,57 +145,15 @@ class TableCollect(table_coverage.TableCoverage):
                     indent=4,
                 )
 
-    def _get_chunks_to_collect(
-        self, data_range: typing.Any | None = None, overwrite: bool = False
-    ) -> tuple[list[typing.Any], list[str]]:
-        import os
-
-        if self.write_range == 'overwrite_all':
-            chunk_ranges = [None]
-            paths = [self.get_file_path(None)]
-
-        else:
-            # get data range
-            if data_range is None:
-                if overwrite:
-                    data_range = [self.get_available_range()]
-                else:
-                    collected_range = self.get_collected_range()
-                    available_range = self.get_available_range()
-                    if collected_range is None:
-                        data_ranges = [available_range]
-                    else:
-                        data_ranges = truck.ops.ranges.get_range_diff(
-                            subtract_this=collected_range,
-                            from_this=available_range,
-                            range_format=self.range_format,
-                        )
-            else:
-                data_ranges = [data_range]
-
-            # partition range into chunks
-            chunk_ranges = truck.ops.ranges.partition_into_chunks(
-                data_ranges, range_format=self.range_format
-            )
-            paths = [
-                self.get_file_path(data_range=chunk_range)
-                for chunk_range in chunk_ranges
-            ]
-
-        # filter existing
-        if not overwrite:
-            new_chunk_ranges: list[typing.Any] = []
-            new_paths = []
-            for chunk_range, path in zip(chunk_ranges, paths):
-                if not os.path.isfile(path):
-                    new_chunk_ranges.append(chunk_range)
-                    new_paths.append(path)
-            chunk_ranges = new_chunk_ranges
-            paths = new_paths
-
-        return chunk_ranges, paths
-
-    def summarize_chunk(self, data_range: typing.Any, path: str) -> None:
+    def _summarize_chunk(self, data_range: typing.Any, path: str) -> None:
         import os
 
         print('collecting', os.path.basename(path))
+
+    def _summarize_collected_chunk(
+        self, df: pl.DataFrame | None, chunk_range: typing.Any, path: str
+    ) -> None:
+        import os
+
+        if df is None:
+            print('could not collect data for', os.path.basename(path))
