9f161d4a5ba991124b0e7fcca200b212cd7d54a1
sslivkoff <stormslivkoff@gmail.com>
2025-05-25 18:20:47 +0000

    convert 4byte from async to sync

--- absorb/datasets/fourbyte.py
+++ absorb/datasets/fourbyte.py
@@ -34,12 +34,8 @@ class FourbyteDatatype(absorb.Table):
         max_id = max(result['id'] for result in data['results'])
         return (0, max_id)
 
-    async def async_collect_chunk(
-        self, data_range: typing.Any
-    ) -> pl.DataFrame | None:
-        return await async_scrape_4byte(
-            url=self.endpoint, data_range=data_range
-        )
+    def collect_chunk(self, data_range: typing.Any) -> pl.DataFrame | None:
+        return scrape_4byte(url=self.endpoint, data_range=data_range)
 
 
 class Functions(FourbyteDatatype):
@@ -54,40 +50,36 @@ def get_tables() -> list[type[absorb.Table]]:
     return [Functions, Events]
 
 
-async def async_scrape_4byte(
+def scrape_4byte(
     url: str,
     data_range: tuple[int, int],
     wait_time: float = 0.1,
     min_id: int | None = None,
 ) -> pl.DataFrame:
-    import aiohttp
+    import requests
     import polars as pl
+    import time
 
     results = []
-    async with aiohttp.ClientSession() as session:
-        while True:
-            # get page
-            async with session.get(url) as response_object:
-                response = await response_object.json()
-                results.extend(response['results'])
+    while True:
+        # get page
+        response = requests.get(url)
+        result: dict[str, typing.Any] = response.json()
+        results.extend(result['results'])
 
-            # scrape only until min_id is reached
-            if min_id is not None:
-                min_result_id = min(
-                    result['id'] for result in response['results']
-                )
-                if min_result_id < min_id:
-                    break
-
-            # get next url
-            url = response['next']
-            if url is None:
+        # scrape only until min_id is reached
+        if min_id is not None:
+            min_result_id = min(result['id'] for result in result['results'])
+            if min_result_id < min_id:
                 break
 
-            # wait between responses
-            if wait_time is not None:
-                import asyncio
+        # get next url
+        url = result['next']
+        if url is None:
+            break
 
-                await asyncio.sleep(wait_time)
+        # wait between responses
+        if wait_time is not None:
+            time.sleep(wait_time)
 
     return pl.DataFrame(results, orient='row')
