ae1cb63113ae30420635edab04c7205db0798e60
sslivkoff <stormslivkoff@gmail.com>
2025-07-12 21:22:56 -0700

    clean toplevel module imports

--- absorb/__init__.py
+++ absorb/__init__.py
@@ -1,8 +1,13 @@
 """python interface for interacting with flashbots mempool dumpster"""
 
-from .types import *
+from .types.table_class import Table
 from . import ops
 from .ops import scan, load, get_available_range, get_collected_range
+
+import typing
+
+if typing.TYPE_CHECKING:
+    from .types.annotations import *
 
 
 __version__ = '0.2.0'
